5d95001c33543f712340ed7c4921b30ff8153c9e
JonathanNiez <jonathanniez07@gmail.com>
2023-02-11 09:56:34 +0800

    first commit

---
+++ marpython/mar.py
@@ -0,0 +1,11 @@
+x = "aggiN"
+true = Mar
+
+
+if Mar is not True:
+    print("False")
+elif true is True:
+        print("True")
+
+else:
+    print("True")
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Yow</h1>
+      <dave>Dave</dave>
+    </div>
+  );
+}
